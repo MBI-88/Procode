008f9ec9e17c0538ef9e07de49b4f36cce71adb4
MBI <ingmbi8807@gmail.com>
2022-05-28 11:33:05 -0400

    Edit Admin site and templates

--- cells/admin.py
+++ cells/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import ShopingCell,ProfileUser
 
 # Register your models here.
+
+@admin.register(ShopingCell)
+class ShopingCellAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(ProfileUser)
+class ProfileUserAdmin(admin.ModelAdmin):
+    pass
