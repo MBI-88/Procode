aa06efcd6b9a54fdd4721472743e6aba306878b7
MBI <ingmbi8807@gmail.com>
2022-05-29 21:26:18 -0400

    Reedit forms

--- cells/forms.py
+++ cells/forms.py
@@ -1,26 +1,26 @@
+from email.mime import image
 from django import forms
-from django.contrib.auth.models import User 
-from .models import ProfileUser
+
 
 # Forms
 
 class LoginForm(forms.Form):
-    username = forms.CharField(max_length=50,attrs={'class':'form'})
-    password = forms.CharField(widget=forms.PasswordInput,attrs={'class':'form'})
+    username = forms.CharField(max_length=50,attrs={'class':'forms'})
+    password = forms.CharField(widget=forms.PasswordInput,attrs={'class':'forms'})
 
 
-class UserRegistrationForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput,attrs={'class':'form'})
-    password2 = forms.CharField(widget=forms.PasswordInput,attrs={'class':'form'})
+class UserRegistrationForm(forms.Form):
+    username = forms.CharField(max_length=25,attrs={'class':'forms'})
+    first_name = forms.CharField(max_length=50,attrs={'class':'forms'})
+    last_name = forms.CharField(max_length=100,attrs={'class':'forms'})
+    password = forms.CharField(widget=forms.PasswordInput,attrs={'class':'forms'})
+    password2 = forms.CharField(widget=forms.PasswordInput,attrs={'class':'forms'})
+    email = forms.EmailField(attrs={'class':'forms'})
+    phone = forms.CharField(max_length=15,attrs={'class':'forms'})
+    image = forms.ImageField(attrs={'class':'forms'})
+    address = forms.CharField(max_length=100,attrs={'class':'forms'})
 
-    def __init__(self,*args, **kwargs) -> None:
-        self.fields['username'].widget_attrs({'class':'form'})
-        self.fields['first_name'].widget_attrs({'class':'form'})
-        self.fields['email'].widget_attrs({'class':'form'})
 
-    class Meta:
-        model = User
-        fields = ('username','first_name','email')
     
     def clean_password2(self) -> str:
         cd = self.cleaned_data
@@ -29,22 +29,5 @@ class UserRegistrationForm(forms.ModelForm):
         
         return cd['password2']
 
-class ProfileEditForm(forms.ModelForm):
-    def __init__(self,*args, **kwargs) -> None:
-        self.fields['image'].widget_attrs({'class':'form'})
-        self.fields['address'].widget_attrs({'class':'form'})
-        self.fields['phone'].widget_attrs({'class':'form'})
 
-    class Meta:
-        model = ProfileUser
-        fields = ('image','address','phone')
 
-class UserEditForm(forms.ModelForm):
-    def __init__(self,*args, **kwargs) -> None:
-        self.fields['username'].widget_attrs({'class':'form'})
-        self.fields['first_name'].widget_attrs({'class':'form'})
-        self.fields['email'].widget_attrs({'class':'form'})
-
-    class Meta:
-        model = User
-        fields = ('username','email') 
--- cells/views.py
+++ cells/views.py
@@ -1,6 +1,98 @@
-from django.shortcuts import render
+from time import time
+from django.shortcuts import redirect, render
+from django.views.generic.edit import CreateView,UpdateView,DeleteView
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.utils import timezone
+from django.views.generic import ListView,DetailView,View
+from django.http import HttpResponse, JsonResponse
+from .forms import LoginForm,UserRegistrationForm
+from django.contrib.auth.views import LoginView,LogoutView
+from django.contrib.auth import authenticate
+from django.core.mail import send_mail
+from django.contrib.auth.models import User
+from .models import ProfileUser
+import hashlib
 
 # Create your views here.
 
+# Route Index
 def index(request:str) -> render:
     return render(request,'base.html')
+
+
+# Route Login
+class LoginUser(LoginView):
+    template_name = 'accounts/registration/login.html'
+    form_class = LoginForm
+    next_page = 'accounts/profile/profile.html'
+    
+  
+ 
+
+# Route Logged out
+class LoggedoutUser(LogoutView):
+    template_name = 'accounts/registration/logged_out.html'
+    next_page = ''
+    title = 'Logged out'
+
+
+# Route Register
+class RgisterUser(View):
+    template_name = 'accounts/registration/register.html'
+    form_class = UserRegistrationForm
+    initial = {
+        'username':'Name of the user in the system',
+        'first_name': 'Name of the real user',
+        'last_name': 'Last name of real user',
+        'password': 'Password',
+        'password2': 'Confirm',
+        'email': 'Email',
+        'phone':'Phone',
+        'image': 'User\'picture',
+        'address': 'Address'
+    }
+
+    def get(self,request:str, *args, **kwargs) -> render:
+        form = self.form_class(initial=self.initial)
+        return render(request,self.template_name,{'form':form})
+
+    
+    def post(self,request:str, *args, **kwargs) -> render:
+        form = self.form_class(request.POST)
+        cd = form.cleaned_data
+        if form.is_valid():
+            user = User.objects.filter(username=cd['username'],password=cd['password'])
+            if (user is None):
+                new_user = User(username=cd['username'],
+                                password=cd['password'],
+                                first_name=cd['first_name'],
+                                last_name=cd['last_name'],
+                                email=cd['email'],
+                                is_active=False)
+                
+                key_token = ''
+                key_datetoken = timezone.now() + timezone.timedelta(1)
+
+                new_user2 = ProfileUser(phone=cd['phone'],
+                                        image=cd['image'],
+                                        address=cd['address'],
+                                        key_token=key_token,
+                                        key_date=key_datetoken)
+                # email_sender
+            else:
+                return HttpResponse('The user {} is in the system'.format(cd['username']))
+        
+        return render(request,self.template_name,{'form':form.errors})
+
+
+
+
+# Route List Items
+class ShowItems(ListView):
+    pass
+
+
+# Route Detail Items
+class DetailItem(DetailView):
+    pass
