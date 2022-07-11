35495ff4d9e4e61013a4a2b6dcf7c4fc84b88411
MBI <ingmbi8807@gmail.com>
2022-07-11 18:05:51 -0400

    changes style in cell index html

---
+++ cells/static/procodejs/animation_index.js
@@ -0,0 +1,31 @@
+'use strict'
+
+function animationOne(event){ 
+    const element3 = document.getElementById('text-inter1')
+    element3.hidden = false
+    element3.className += ' procode-pres3'
+    
+    const element4 = document.getElementById('img-inter1')
+    element4.hidden = false
+    element4.className += ' procode-pres4'
+    
+}
+
+function animationTwo(event){
+    const element3 = document.getElementById('text-inter2')
+    element3.hidden = false
+    element3.className += ' procode-pres5'
+    
+    const element4 = document.getElementById('img-inter2')
+    element4.hidden = false
+    element4.className += ' procode-pres6'
+}
+
+function animationThree(event){
+    const element = document.getElementById('text-bg-2')
+    element.hidden = false
+    element.className += ' text-middle-2'
+
+}
+
+
