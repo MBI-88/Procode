f6a4ed04dd55cc0e7503a810be50af8ffccb8519
MBI <ingmbi8807@gmail.com>
2022-10-05 20:05:39 -0300

    updated styles

--- cells/static/procodejs/animation_index.js
+++ cells/static/procodejs/animation_index.js
@@ -4,12 +4,10 @@
 const dict = {
     'block-1': document.getElementById('block-1').offsetTop,
     'block-2': document.getElementById('block-2').offsetTop,
-    'block-3': document.getElementById('block-3').offsetTop,
 }
 const flag = {
     'flag-1': false,
     'flag-2': false,
-    'flag-3': false,
 }
 const elementCarousel = document.getElementById('img-inter1')
 
@@ -31,15 +29,6 @@ function animationTwo() {
     element.classList.add('procode-pres3')
     element.hidden = false
 }
-function animationThree() {
-    const element = document.getElementById('text-inter2')
-    element.classList.add('procode-pres4')
-    element.hidden = false
-
-    const element2 = document.getElementById('img-inter2')
-    element2.classList.add('procode-pres4')
-    element2.hidden = false
-}
 
 // Wrappers
 
@@ -53,11 +42,7 @@ const handleScroll = async () => {
         flag['flag-2'] = true
         elementCarousel.children[0].children[0].classList.remove('procode-pres2')
     }
-    if (dict["block-3"] - 700 < document.documentElement.scrollTop && !flag['flag-3']) {
-        animationThree()
-        flag['flag-3'] = true
-    }
-    if (flag['flag-1'] && flag['flag-2'] && flag['flag-3']) {
+    if (flag['flag-1'] && flag['flag-2']) {
         window.removeEventListener('scroll', handleScroll, false)
     }
 }
