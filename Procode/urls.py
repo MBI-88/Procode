1132e5935b3533b358e915d4f4bc4f107f773cab
MBI <ingmbi8807@gmail.com>
2022-10-11 00:07:49 -0300

    updating production

--- Procode/urls.py
+++ Procode/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import handler403,handler500,handler404,handler400
 
 urlpatterns = [
     path('pr0c0d3-admin/', admin.site.urls),
