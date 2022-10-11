30d45c6e74b4cd4aabf1c0e1fc29e4fffa3a76ba
MBI <ingmbi8807@gmail.com>
2022-10-11 13:58:21 -0300

    make it better

--- procode/settings/production_setting.py
+++ procode/settings/production_setting.py
@@ -30,7 +30,7 @@ db_from_env = dj_database_url.config(conn_max_age=500)
 
 DATABASES['default'].update(db_from_env)
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
