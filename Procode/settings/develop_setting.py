a65332598b037b24546eb5b2dbe647d4cf165552
MBI <ingmbi8807@gmail.com>
2022-10-11 18:39:14 -0300

    update setting develop email

--- Procode/settings/develop_setting.py
+++ Procode/settings/develop_setting.py
@@ -4,8 +4,12 @@ from .base_setting import *
 DEBUG = config('DEBUG',cast=bool,default=True)
 SECRET_KEY = config('SECRET_KEY')
 EMAIL_BACKEND = config('EMAIL_BACKEND')
-EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_PORT = config('EMAIL_PORT',cast=int)
+EMAIL_HOST = config('EMAIL_HOST') # Put your e-mail admin
+EMAIL_PORT = config('EMAIL_PORT',cast=int) # Put your port 
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS',default=True,cast=bool)
+EMAIL_USE_SSL = config('EMAIL_USE_SSL',default=True,cast=bool)
 
 DATABASES = {
     'default': {
