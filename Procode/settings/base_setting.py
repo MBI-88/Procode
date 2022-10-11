d93e42b610f2ea63572ec05bce8ce63baf47c52f
MBI <ingmbi8807@gmail.com>
2022-10-11 08:08:19 -0300

    change settings 2

--- Procode/settings/base_setting.py
+++ Procode/settings/base_setting.py
@@ -45,6 +45,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 CORS_ORIGIN_WHITELIST = (
