2102981fd934964b6d6d92e35cdbf41def24da7a
MBI <ingmbi8807@gmail.com>
2022-10-11 19:00:08 -0300

    Debug false

--- procode/wsgi.py
+++ procode/wsgi.py
@@ -1,6 +1,6 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'procode.settings.develop_setting')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'procode.settings.production_setting')
 
 application = get_wsgi_application()
