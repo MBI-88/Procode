2ac8a26c568e09d6480c85ef507c6f5a5c70946c
MBI <ingmbi8807@gmail.com>
2022-10-10 23:29:05 -0300

    dele staticsfiles

--- Procode/settings/base_setting.py
+++ Procode/settings/base_setting.py
@@ -44,7 +44,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    
 ]
 
 CORS_ORIGIN_WHITELIST = (
@@ -109,10 +109,3 @@ LOGOUT_URL = 'cells:logout'
 
 PASSWORD_RESET_TIMEOUT_DAYS = 86400 #s
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
