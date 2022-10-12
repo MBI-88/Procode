a3d7da88cc209e8f1232583859ea6f807608d253
MBI <ingmbi8807@gmail.com>
2022-10-11 21:11:21 -0300

    problem resolved

--- procode/urls.py
+++ procode/urls.py
@@ -10,9 +10,8 @@ urlpatterns = [
     path('api/', include('cells.api.urls', namespace='cells_api')),
 ]
 if settings.DEBUG:
-    #urlpatterns += static(settings.MEDIA_URL,
-     #                     document_root=settings.MEDIA_ROOT)
-     pass
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
 else:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
