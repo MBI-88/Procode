744dad75581270047ea03314195e57fa363453d7
MBI <ingmbi8807@gmail.com>
2022-10-12 21:29:20 -0300

    Add sitemap

---
+++ cells/sitemap.py
@@ -0,0 +1,16 @@
+from django.contrib.sitemaps import Sitemap
+from django.urls import reverse
+
+# Class
+
+
+class StaticViewSitemap(Sitemap):
+    priority = 0.5
+    changefreq = 'daily'
+
+    def items(self) -> list:
+        return ['cells:info', 'cells:who', 'cells:contact', 'cells:dashboar_list', 
+                'cells:index', 'cells_api:showitems_api',]
+
+    def location(self, item: str) -> reverse:
+        return reverse(item)
